35c3303cf23399548b57bfe94955ad62262af34a
kumarmudur <kumarmudur10@gmail.com>
2024-02-19 01:39:03 -0500

    [Backend] initial commit of backend code

---
+++ backend/src/main/java/com/kumarmudur/subscriptionmanagment/SubscriptionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.kumarmudur.subscriptionmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionManagementApplication.class, args);
+	}
+
+}
